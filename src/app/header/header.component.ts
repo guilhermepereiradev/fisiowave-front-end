376928fd26ac7b05e08d92a6a5afdaf028cc65dc
ArthLDP <arthurlourencodp@gmail.com>
2024-04-28 17:08:08 -0300

    feat: change home layout

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { MatToolbar } from '@angular/material/toolbar'
 import { MatButton } from '@angular/material/button'
 import { RouterModule } from '@angular/router'
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [MatToolbar, MatButton, RouterModule],
+  imports: [MatToolbar, MatButton, RouterModule, MatIconModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })

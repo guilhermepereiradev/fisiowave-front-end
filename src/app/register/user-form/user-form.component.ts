66e1046784dc1ea6e5d66d21a7d60c748d075b17
ArthLDP <arthurlourencodp@gmail.com>
2024-04-29 17:24:23 -0300

    feat: add confirm password functionality

--- src/app/register/user-form/user-form.component.ts
+++ src/app/register/user-form/user-form.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { AbstractControl, FormBuilder, FormGroup, ReactiveFormsModule, ValidationErrors, ValidatorFn, Validators } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule, } from '@angular/material/input'
 import { MatDatepickerModule } from '@angular/material/datepicker';
@@ -19,18 +19,58 @@ import { Router } from '@angular/router';
   templateUrl: './user-form.component.html',
   styleUrl: './user-form.component.css'
 })
-export class UserFormComponent implements OnInit{;
-
+export class UserFormComponent implements OnInit {
   constructor(
     private dialog: MatDialog,
     private patientService: PatientService,
     private cityService: CityService,
     private fb: FormBuilder,
     private router: Router
-  ) {}
+  ) { }
 
   cities?: City[];
   citiesNameList?: string[] = [];
+
+  confirmPasswordValidator(group: FormGroup) {
+    const passwordControl = group.get('password');
+    const confirmPasswordControl = group.get('confirmPassword');
+
+    if (passwordControl && confirmPasswordControl) {
+      const password = passwordControl.value;
+      const confirmPassword = confirmPasswordControl.value;
+
+      if (password !== "" && confirmPassword !== "") {
+        if (password === confirmPassword) {
+          confirmPasswordControl.setErrors(null);
+          return null;
+        } 
+        else {
+          confirmPasswordControl.setErrors({ mismatch: true });
+        }
+      }
+    }
+    return null;
+  }
+
+  userForm: FormGroup = this.fb.group({
+    name: ["", [Validators.required, Validators.minLength(3)]],
+    phoneNumber: ["", [Validators.required, Validators.pattern("[12-99]{1}[0-9]{10}")]],
+    birthDate: ["", [Validators.required]],
+    email: ["", [Validators.required, Validators.email]],
+    password: ["", [Validators.required]],
+    confirmPassword: ["", [Validators.required]],
+
+    zipCode: ["", [Validators.minLength(8), Validators.maxLength(8)]],
+    street: [""],
+    number: [""],
+    complement: [""],
+    neighborhood: [""],
+    state: [""],
+    city: [""],
+  },
+    {
+      validators: this.confirmPasswordValidator
+    })
 
   ngOnInit(): void {
     this.cityService.getAllCities().subscribe({
@@ -40,26 +80,9 @@ export class UserFormComponent implements OnInit{;
     this.cities?.map(city => this.citiesNameList?.push(city.name));
   }
 
-
-  userForm: FormGroup = this.fb.group({
-    name: ["", [Validators.required, Validators.minLength(3)]],
-    phoneNumber: ["", [Validators.required, Validators.pattern("[12-99]{1}[0-9]{10}")]],
-    birthDate: ["", [Validators.required]],
-    email: ["", [Validators.required, Validators.email]],
-    password: ["", [Validators.required]],
-
-    zipCode: ["", [Validators.minLength(8), Validators.maxLength(8)]],
-    street: [""],
-    number: [""],
-    complement: [""],
-    neighborhood: [""],
-    state: [""],
-    city: [""],
-  })
-
   register(): void {
     const isFormValid = this.userForm.valid;
-    
+
     if (!isFormValid) {
       this.userForm.markAllAsTouched();
       this.dialog.open(RequestDialogComponent, {
@@ -67,9 +90,9 @@ export class UserFormComponent implements OnInit{;
           message: "Preencha todos os campos obrigatórios corretamente"
         }
       });
-    } else {    
+    } else {
       let userData = this.userForm.value;
-      
+
       let newUser = new UserRequest(userData.name, userData.phoneNumber, userData.birthDate, userData.email);
       newUser.address = new AddressRequest(userData.zipCode, userData.street, userData.number, userData.neighborhood, userData.complement, new CityIdRequest(userData.city))
 

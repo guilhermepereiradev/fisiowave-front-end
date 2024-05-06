3712bc509f2c7b5c7536074bef2f52c5a4fb7525
ArthLDP <arthurlourencodp@gmail.com>
2024-05-06 18:23:27 -0300

    fix: stop interceptor to send token every request, only send when there is local storage

--- src/app/login/services/patient.interceptor.ts
+++ src/app/login/services/patient.interceptor.ts
@@ -3,11 +3,16 @@ import { HttpInterceptorFn } from '@angular/common/http';
 export const patientInterceptor: HttpInterceptorFn = (req, next) => {
   const authToken = localStorage.getItem("loginToken");
 
-  const authReq = req.clone({
-    setHeaders: {
-      Authorization: `Bearer ${authToken}`
-    }
-  });
-
-  return next(authReq);
+  if (authToken) {
+    const authReq = req.clone({
+      setHeaders: {
+        Authorization: `Bearer ${authToken}`
+      }
+    });
+  
+    return next(authReq);
+  }
+  else {
+    return next(req);
+  }
 };
